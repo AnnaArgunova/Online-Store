80b2a6d31048567e4d70dc474a2307d5e48f7f60
AnnaArgunova <anna.argunova@gmail.com>
2021-02-11 23:10:42 +0200

    Updates

---
+++ precache-manifest.dc6e4aa1a8cc0084940981b2fc80e810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd335924407716d971c01f3a61220f6",
+    "url": "/Online-Store/index.html"
+  },
+  {
+    "revision": "9416850ad9e03fc9e900",
+    "url": "/Online-Store/static/css/2.3c4e6f21.chunk.css"
+  },
+  {
+    "revision": "0a7c3254e8d41af118ea",
+    "url": "/Online-Store/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9416850ad9e03fc9e900",
+    "url": "/Online-Store/static/js/2.9bdd99b0.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/Online-Store/static/js/2.9bdd99b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7c3254e8d41af118ea",
+    "url": "/Online-Store/static/js/main.9e66b05c.chunk.js"
+  },
+  {
+    "revision": "c87230586fc61862e25f",
+    "url": "/Online-Store/static/js/runtime-main.b5472b70.js"
+  }
+]);
